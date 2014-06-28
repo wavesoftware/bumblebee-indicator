8f58f4e2ad03049c7b84b69959fa7c7884ace9f0
Krzysztof Suszyński <krzysztof.suszynski@wavesoftware.pl>
2014-06-28 12:07:28 +0200

    Auto start on debian

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python2
 
 from setuptools import setup
+from distutils.command.install_data import install_data
+
+cmdclasses = {'install_data': install_data}
+data_files = [
+	('/etc/xdg/autostart', ['etc/xdg/autostart/bumblebee-indicator.desktop']),
+	('/usr/share/applications', ['etc/xdg/autostart/bumblebee-indicator.desktop']),
+	('/usr/share/pixmaps', ['bumblebee_indicator/resources/bumblebee-icon-active.png']),
+]
 
 setup(
   name         = 'bumblebee-indicator',
-  version      = '0.1.0',
+  version      = '0.1.2',
   description  = 'A Unity desktop bumblebee indicator - switch dual screens, enable 3d mode',
   url          = 'http://github.com/storborg/funniest',
   author       = 'Wave Software',
@@ -13,6 +21,8 @@ setup(
   packages     = ['bumblebee_indicator'],
   zip_safe     = True,
   scripts      = ['bin/bumblebee-indicator'],
+  cmdclass     = cmdclasses,
+  data_files   = data_files, 
   package_data = { 
     '': ['resources/*']
   }
